0d308bf31fae6794c24be6f8931d95462d0edbde
SonataD <zkp0s@users.noreply.github.com>
2015-02-24 23:33:06 -0600

    Oops

--- source/main.d
+++ source/main.d
@@ -1,21 +1,11 @@
 ﻿module main;
-
 import std.stdio;
 import maud.vm.util;
-import maud.test.vm;
-extern (C) void doStuff(){
-	static int[] data = [-1,-1,-1];
-	data[0] = 0;
-}
+import maud.test.z80;
 
-extern (C) void doStuff2(){
-	static int[] data = [-1,-1,-1];
-	data[0] = 0;
-}
 
 void main(string[] args)
 {
-	// Prints "Hello World" string in console
 	testVM();
 	stdin.readln();
 }
