e3c12da3880d2c0681e810934836f18ed6d91da7
SonataD <zkp0s@users.noreply.github.com>
2015-05-23 21:09:32 -0500

    Oops, I think I will never understand git

--- source/main.d
+++ source/main.d
@@ -1,13 +1,43 @@
 ﻿module main;
 import std.stdio;
-import maud.vm.util;
+import std.getopt;
+import maud.vm.z80;
+import maud.vm.arrayMem;
+import maud.glue.maudInterface;
+import maud.ui.app;
 import maud.test.z80;
+import dlangui;
+mixin APP_ENTRY_POINT;
+
+/// entry point for dlangui based application
+extern (C) int UIAppMain(string[] args) {
+
+	// create window
+	bool doTests;
+	getopt(args, "test", &doTests);
+
+	if(doTests) {pmain; return 0;}
+
+	auto c = new ApplicationContext(new Z80VM( new ArrayMem(64*1024 + 32)));
+
+	auto app = new MainApp(c);
+	//FileDialog dlg = createFileDialog("gg");
+//	dlg.show();
+	// run message loop
+	return Platform.instance.enterMessageLoop();
+}
 
 
-void main(string[] args)
+
+void pmain()
 {
+	//auto mem = arrayMem.create(64*1024);
+	//I8Reader.read(mem,stdin.byLine);
+	//StdDump dumper = new StdDump();
+	//dumper.setWordSize(2);
+	//dumper.push(mem.getAddress(0),0xFF);
+
 	writeln("Executing test cases...");
 	writeln("[passed, failed]: ",testVM());
-	stdin.readln();
 }
 
